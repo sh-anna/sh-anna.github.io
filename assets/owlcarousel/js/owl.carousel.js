87a1c188f1ca39fa4be28d73acaae281dcb5f4df
Sharipkin Anna <sharipkinanna@gmail.com>
2021-06-21 10:22:15 +0300

    Add files via upload

--- assets/owlcarousel/js/owl.carousel.js
+++ assets/owlcarousel/js/owl.carousel.js
@@ -1,10 +1,10 @@
 /*
- *  jQuery OwlCarousel v1.3.3
+ *  
  *
- *  Copyright (c) 2013 Bartosz Wojciechowski
- *  http://www.owlgraphic.com/owlcarousel/
+ *  
+ *  
  *
- *  Licensed under MIT
+ *  
  *
  */
 
