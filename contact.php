d5a10936d3617dd72010a3c0e890ea4e545216dc
Sharipkin Anna <sharipkinanna@gmail.com>
2021-06-22 16:43:45 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,8 +17,8 @@
 		$mail->isSMTP();                                            
 		$mail->Host = 'smtp.gmail.com';                     
 		$mail->SMTPAuth = true;                                  
-		$mail->Username = 'sharipkinanna@gmail.com';                     
-		$mail->Password = '1609tygarin1';                               
+		$mail->Username = 'myGmailAdress';                     
+		$mail->Password = 'myGmailPassword';                               
 		$mail->SMTPSecure = 'tls';        
 		$mail->Port = 465; 
 		$mail->isHTML(true); 
