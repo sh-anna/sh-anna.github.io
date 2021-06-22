63599fe8cf81cfeda482d5b18f4f181b99a41e0b
Sharipkin Anna <sharipkinanna@gmail.com>
2021-06-22 16:41:18 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,40 +15,30 @@
 		//bdika
 		//smtp settings
 		$mail->isSMTP();                                            
-		$mail->Host = 'smtp.example.com';                     
+		$mail->Host = 'smtp.gmail.com';                     
 		$mail->SMTPAuth = true;                                  
 		$mail->Username = 'myGmailAdress';                     
 		$mail->Password = 'myGmailPassword';                               
 		$mail->SMTPSecure = 'tls';        
 		$mail->Port = 465; 
-
-		//email settings
 		$mail->isHTML(true); 
-		$mail->setFrom($email, $name); 
-		$mail->addAddress('sharipkinanna@gmail.com');
-		$mail->Subject = ('$email ($subject)'); 
-		$mail->Body = $message;
+		
 		//sofbdika
 
 
 		if (isset($_POST["submit"])) {
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$message = $_POST['message'];
+			$from = 'Contact with your Client - Contact Form'; 
+			$to = 'sharipkinanna@gmail.com'; 
+			$subject = $_POST['subject']; 
 		
-		
-		
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Contact with your Client - Contact Form'; 
-		$to = 'sharipkinanna@gmail.com'; 
-		$subject = $_POST['subject']; 
-		
-		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+			$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
 	
 
-		mail($to, $subject, $body, $from) or die("Error!");
-		header("location: thank-you.html");
-		
-		
-	}
+			mail($to, $subject, $body, $from) or die("Error!");
+			header("location: thank-you.html");	
+		}
 	
 ?>
