826a95bbe587bca3d4c1ca2a2f12e6448e9bccd5
Sharipkin Anna <sharipkinanna@gmail.com>
2021-06-22 16:24:38 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,9 +1,26 @@
 <?php
 	if (isset($_POST["submit"])) {
+		
+		//bdika
+		//smtp settings
+		$mail->isSMTP();                                            
+		$mail->Host = 'smtp.example.com';                     
+		$mail->SMTPAuth = true;                                  
+		$mail->Username = 'sharipkinanna@gmail.com';                     
+		$mail->Password = '1609tygarin1';                               
+		$mail->SMTPSecure = 'tls';        
+		$mail->Port = 465; 
+
+		//email settings
+		$mail->isHTML(true); 
+		$mail->setFrom($email, $name); 
+		$mail->addAddress('sharipki
+		//sofbdika
+		
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
-		$from = 'Gridus Contact Form'; 
+		$from = 'Contact with your Client - Contact Form'; 
 		$to = 'sharipkinanna@gmail.com'; 
 		$subject = $_POST['subject']; 
 		
